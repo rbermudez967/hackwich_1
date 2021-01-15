254a12ac06ea5b6d89b6859078284008bca0d0b7
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-01-15 11:08:16 -1000

    initial commit
    initial commit.  hello!

---
+++ Hackwich1/Hackwich1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackwich1
+//
+//  Created by Randy Bermudez on 1/12/21.
+//  Copyright © 2021 Marion Ano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
